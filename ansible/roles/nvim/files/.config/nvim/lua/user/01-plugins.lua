eb095c67f28f20d0b72dd53de018cbf634d5a161
CarefulNow <daniel+ghp@dodgitech.com>
2024-03-24 22:37:52 +0100

    Include java 22 in asdf

--- ansible/roles/nvim/files/.config/nvim/lua/user/01-plugins.lua
+++ ansible/roles/nvim/files/.config/nvim/lua/user/01-plugins.lua
@@ -38,6 +38,7 @@ return require('packer').startup(function(use)
       {'hrsh7th/nvim-cmp'},
       {'hrsh7th/cmp-nvim-lsp'},
       {'L3MON4D3/LuaSnip'},
+      {'mfussenegger/nvim-jdtls'},
     }
   }
 
