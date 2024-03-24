8f6325fa3e65aaceb1e7a7ad2ab17c4acbd7acda
CarefulNow <daniel+ghp@dodgitech.com>
2024-03-24 22:29:20 +0100

    Fix nvim to use mason and lsp-zero
    Lsp-Zero helps bring together a bunch of lsp plugins and makes them play
    nice together. the nvim lsp plugins provided by william bowman are
    deprecated so moving to the newer better mason.

--- ansible/roles/nvim/files/.config/nvim/lua/user/03-formatting.lua
+++ ansible/roles/nvim/files/.config/nvim/lua/user/03-formatting.lua
@@ -1,3 +1,9 @@
 -- Set TOC depth to 2 in markdown
 vim.g.vmt_max_level = 3
 
+-- Set Tab to 4 spaces. Sets "tabs" to 8 chars so they are obvious when they appear
+vim.opt.shiftwidth = 4
+vim.opt.smarttab = true
+vim.opt.expandtab = true
+vim.opt.tabstop = 8
+vim.opt.softtabstop = 0
--- ansible/roles/nvim/files/.config/nvim/lua/user/04-lsp-configuration.lua
+++ ansible/roles/nvim/files/.config/nvim/lua/user/04-lsp-configuration.lua
@@ -1,43 +1,50 @@
-require("nvim-lsp-installer").setup({
-    automatic_installation = true, -- automatically detect which servers to install (based on which servers are set up via lspconfig)
-    ensure_installed = {
-	"ansiblels",                      --ansible
-	"bashls",                         --bash
-	"clangd",                         --C/C++
-	"cmake",                          --cmake
-	"cssls",                          --css
-	"codeqlls",                       --codeql
-	"cucumber_language_server",       --cucumber
-	"dockerls",                       --docker
-	"gopls",                          --go
-	"graphql",                        --graphql
-	"html",                           --html
-	"jsonls",                         --json
-	"jdtls",                          --java
-	"tsserver",                       --javascript
-	"kotlin_language_server",         --Kotlin
-	"texlab",                         --latex
-	"remark_ls",                      --markdown
-	"perlnavigator",                  --perl
-	"puppet",                         --puppet
-	"jedi_language_server",           --python
-	"solargraph",                     --ruby
-	"rust_analyzer",                  --rust
-	"sqlls",                          --sql
-	"sumneko_lua",                    --lua
-	"taplo",                          --TOML
-	"terraformls",                    --terraform
-	"tflint",                         --terraform linting
-	"tsserver",                       --Typescript
-	"lemminx",                        --XML
-    "yamlls"                          --YAML
-    },
-    ui = {
-        icons = {
-            server_installed = "✓",
-            server_pending = "➜",
-            server_uninstalled = "✗"
-        }
-    }
-})
+local lsp_zero = require('lsp-zero')
 
+lsp_zero.on_attach(function(client, bufnr)
+	-- see :help lsp-zero-keybindings
+	-- to learn the available actions
+	lsp_zero.default_keymaps({buffer = bufnr})
+end)
+
+-- to learn how to use mason.nvim with lsp-zero
+-- read this: https://github.com/VonHeikemen/lsp-zero.nvim/blob/v3.x/doc/md/guide/integrate-with-mason-nvim.md
+require('mason').setup({})
+require('mason-lspconfig').setup({
+	ensure_installed = {
+		"ansiblels",                      --ansible
+		"bashls",                         --bash
+		"clangd",                         --C/C++
+		"cmake",                          --cmake
+		"cssls",                          --css
+		"codeqlls",                       --codeql
+		"cucumber_language_server",       --cucumber
+		"dockerls",                       --docker
+		"gopls",                          --go
+		"graphql",                        --graphql
+		"html",                           --html
+		"jsonls",                         --json
+		"jdtls",                          --java
+		"java-test",                      --more java
+		"java-debug-adapter",             --even more java
+		"tsserver",                       --javascript
+		"kotlin_language_server",         --Kotlin
+		"texlab",                         --latex
+		"remark_ls",                      --markdown
+		"perlnavigator",                  --perl
+		"puppet",                         --puppet
+		"jedi_language_server",           --python
+		"solargraph",                     --ruby
+		"rust_analyzer",                  --rust
+		"sqlls",                          --sql
+		"sumneko_lua",                    --lua
+		"taplo",                          --TOML
+		"terraformls",                    --terraform
+		"tflint",                         --terraform linting
+		"tsserver",                       --Typescript
+		"lemminx",                        --XML
+		"yamlls"                          --YAML
+	},
+	handlers = {
+		lsp_zero.default_setup,
+	},
+})
